68059978a94adc54ad675351fa411534b0f4841a
lingyunmo <2087789639@qq.com>
2023-12-05 15:28:22 +0800

    Update lesson2

--- src/main/java/com/ling/jsb/controller/BookController.java
+++ src/main/java/com/ling/jsb/controller/BookController.java
@@ -1,21 +1,17 @@
 package com.ling.jsb.controller;
 
 import com.ling.jsb.pojo.Book;
-import com.ling.jsb.pojo.Result;
-import com.ling.jsb.pojo.ResultUtil;
 import com.ling.jsb.service.BookService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/myBook")
-@Slf4j
 public class BookController {
     private final BookService bookService;
 
@@ -26,22 +22,52 @@ public class BookController {
 
     @GetMapping("/book")
     public String findBookById(Integer id, Model model) {
-        Book book = bookService.findBookById(id);
-        model.addAttribute("book", book);
+        model.addAttribute("book", bookService.findBookById(id));
         return "book";
     }
 
     @GetMapping("/books")
     public String getAllBooks(Model model) {
-        List<Book> books = bookService.getAllBooks();
-        model.addAttribute("books", books);
+        model.addAttribute("books", bookService.getAllBooks());
         return "books";
     }
 
-    @GetMapping("/book2")
-    public Result findBookById2(Integer id) {
-        log.info("id= {}", id);
-        Book book = bookService.findBookById(id);
-        return ResultUtil.success(book);
+    @GetMapping("/toAddBook")
+    public String toAddBook() {
+        return "addBook";
+    }
+
+    @PostMapping("/addBook")
+    public String addBook(@RequestParam("bookName") String bookName,
+                          @RequestParam("press") String press,
+                          @RequestParam("author") String author,
+                          Model model) {
+        bookService.insertBook(new Book(null, bookName, press, author));
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books";
+    }
+
+    @GetMapping("/toUpdateBook")
+    public String updateBook(@RequestParam Integer id, Model model) {
+        model.addAttribute("book", bookService.findBookById(id));
+        return "updateBook";
+    }
+
+    @PostMapping("/updateBook")
+    public String UpdateBook(@RequestParam("bookId") Integer id,
+                             @RequestParam("bookName") String bookName,
+                             @RequestParam("press") String press,
+                             @RequestParam("author") String author,
+                             Model model) {
+        bookService.updateBook(new Book(id, bookName, press, author));
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books";
+    }
+
+    @GetMapping("/deleteBook")
+    public String deleteBook(@RequestParam Integer id, Model model) {
+        bookService.deleteBookById(id);
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books";
     }
 }
